61d6343ab676c32211a98c3693139cfdeb12700c
nievecmau <143980547+nievecmau@users.noreply.github.com>
2024-04-14 20:23:53 -0600

    Login init

---
+++ src/test/java/indigo/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package indigo.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
